eae814a48375013e9ff4c27b446ef2b6b7da097a
Ghaliahsalman95 <159685763+Ghaliahsalman95@users.noreply.github.com>
2024-02-13 14:21:01 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -17,12 +17,12 @@ If the number is a multiple of both 3 and 5, you need to print "FizzBuzz"
 instead of that number*/
         System.out.println("-----------Exercise 1--------------");
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            } else if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0 ) {
+                System.out.println("Fizz");
             } else System.out.println(i);
         }
         //--------------------------------------Exercise 2----------
